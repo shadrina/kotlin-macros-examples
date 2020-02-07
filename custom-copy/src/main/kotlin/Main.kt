04be4f3d9004ff6502931bdaa3269b66fe5c403f
Anastasiya Shadrina <a.shadrina5@mail.ru>
2020-02-07 07:38:24 +0700

    Refactor macros code

--- custom-copy/src/main/kotlin/Main.kt
+++ custom-copy/src/main/kotlin/Main.kt
@@ -11,25 +11,32 @@ import kotlin.meta.Node.Decl.Func.*
 annotation class CustomCopy {
     operator fun invoke(klass: Structured): Node {
         val primaryParams = klass.primaryConstructor?.params?.map { toFuncParam(it.name, it.type!!) } ?: listOf()
-        val propertiesAsParams = klass.members.filterIsInstance<Property>().mapNotNull {
-            val v = it.vars.singleOrNull()
-            if (v?.type == null) null else toFuncParam(v.name, v.type!!)
-        }
-        val params = primaryParams + propertiesAsParams
+        val propertiesAsParams = klass.members.filterIsInstance<Property>()
+            .filter { !it.readOnly && !it.isPrivate() }
+            .mapNotNull {
+                val v = it.vars.singleOrNull()
+                if (v?.type == null) null else toFuncParam(v.name, v.type!!)
+            }
 
-        val constructorCall = qe"${klass.name}()".copy(args = primaryParams.map { toValueArg(it.name) })
-        val resultDecl = qd"val result = $constructorCall"
-        val stmts = mutableListOf<Stmt>(Stmt.Decl(resultDecl))
-        propertiesAsParams.forEach {
-            val valueSetter = qe"result.${it.name} = ${it.name}"
-            stmts.add(Stmt.Expr(valueSetter))
-        }
-        val returnExpr = qe"return result"
-        stmts.add(Stmt.Expr(returnExpr))
+        val constructorArgs = primaryParams.map { toValueArg(it.name) }
+        val constructorCall = qe"${klass.name}()".copy(args = constructorArgs)
+        val resultDecl = qd"val result = $constructorCall".toStmt()
+        val setters = propertiesAsParams.map { qe"result.${it.name} = ${it.name}".toStmt() }
+        val returnExpr = qe"return result".toStmt()
+        val stmts = listOf<Stmt>() + resultDecl + setters + returnExpr
 
-        val copy = qd"fun copy() {}".copy(params = params, body = bodyFromStmts(stmts), type = typeFromName(klass.name))
+        val copy = qd"fun copy() {}".copy(
+            params = primaryParams + propertiesAsParams,
+            type = typeFromName(klass.name),
+            body = Body.Block(Block(stmts))
+        )
         return klass.copy(members = klass.members + copy)
     }
+
+    private fun Expr.toStmt() = Stmt.Expr(this)
+    private fun Decl.toStmt() = Stmt.Decl(this)
+
+    private fun Property.isPrivate() = mods.any { it is Modifier.Lit && it.keyword == Modifier.Keyword.PRIVATE }
 
     private fun toFuncParam(name: Name, type: Type) = Param(
         mods = listOf(),
@@ -42,13 +49,21 @@ annotation class CustomCopy {
             rhs = name
         )
     )
-
-    private fun toValueArg(name: Name) = ValueArg(name = null, asterisk = false, expr = name)
-
-    private fun bodyFromStmts(stmts: List<Stmt>) = Body.Block(Block(stmts))
+    private fun toValueArg(name: Name) = ValueArg(
+        name = null,
+        asterisk = false,
+        expr = name
+    )
 
     private fun typeFromName(name: Name) = Type(
         mods = listOf(),
-        ref = TypeRef.Simple(pieces = listOf(TypeRef.Simple.Piece(name = name, typeParams = listOf())))
+        ref = TypeRef.Simple(
+            pieces = listOf(
+                TypeRef.Simple.Piece(
+                    name = name,
+                    typeParams = listOf()
+                )
+            )
+        )
     )
 }
--- custom-copy/src/test/kotlin/Main.kt
+++ custom-copy/src/test/kotlin/Main.kt
@@ -1,12 +1,13 @@
 @macro:CustomCopy
-open class NeedCopy(val c: String) {
-    var a: Int = 1
-    var b: Int = 2
+open class NeedCopy(val a: String) {
+    var b: Int = 1
+    var c: Int = 2
+    private var p: Int = 3
 
-    override fun toString(): String = "a=$a, b=$b, c=$c"
+    override fun toString(): String = "a=$a, b=$b, c=$c, p=$p"
 }
 
 fun main() {
-    val needCopy = NeedCopy("abc")
-    println(needCopy.copy(a = 42))
+    val needCopy = NeedCopy("original")
+    println(needCopy.copy(a = "copy"))
 }
